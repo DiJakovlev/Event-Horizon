664ef71b021831805f3ed542e71d8667fb5cc0fd
Kasutaja <jakovlevd7@gmail.com>
2023-06-16 14:24:13 +0300

    Fixed errors in views

--- event/urls.py
+++ event/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('events/', EventListView.as_view(), name='event-list'),
     path('events/<int:pk>', EventView.as_view(), name='detail-view'),
     path('events/<int:pk>/purchase/', TicketPurchaseView.as_view(), name='ticket-purchase'),
-    path('event/<int:pk>/purchase/conformation/', PurchaseConfirmationView.as_view(), name='purchase-conformation')
+    path('event/<int:pk>/purchase/confirmation/', PurchaseConfirmationView.as_view(), name='purchase-confirmation')
 ]
